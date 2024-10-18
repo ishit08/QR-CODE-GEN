6d26beedc99fb30ff49f892f777535e7174a9208
sdixit18 <saurabh.dixit18@gmail.com>
2024-10-18 13:06:56 +0530

    fixed homepage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 "use client";
-import { useState } from "react";
+
+import ComingSoon from './components/ui/ComingSoon';
 
 const WelcomeComponent = () => {
   return (
-    <div className="flex items-center justify-center mt-20">
-      <h1 className="text-black text-xl font-extrabold tracking-wide drop-shadow-lg">
-        Welcome to CodeSnaps!
-      </h1>
+    <div>
+        <ComingSoon />
     </div>
   );
 };
