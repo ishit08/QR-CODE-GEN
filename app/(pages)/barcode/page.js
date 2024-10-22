d5ab856acc2bced89487b21826ff8f90d28784e2
Gajendra B <banjaragajendra01@gmail.com>
2024-10-22 18:00:47 +0530

    qr page not open without login

--- app/(pages)/barcode/page.js
+++ app/(pages)/barcode/page.js
@@ -1,11 +1,27 @@
 "use client";
 
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from "next/navigation";
 import ComingSoon from '../../components/ui/ComingSoon';
 
 export default function BarcodePage() {
-    return (
-        <div>
-            <ComingSoon />
-        </div>
-    );
+
+  const { data: session, status } = useSession(); // Use useSession to get session status
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      router.push("/login") 
+    }
+  }, [status, router]);
+
+    if (status === 'authenticated') {
+        return (
+            <div>
+                <ComingSoon />
+            </div>
+        );
+    }
+    return null;
 }
--- app/(pages)/scanner/page.js
+++ app/(pages)/scanner/page.js
@@ -3,6 +3,8 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from "next/navigation";
 import CameraScanner from '../../components/scanner/CameraScanner';
 import Tab from '../../components/ui/tab'; // Import the reusable Tab component
 import '../../styles/scanner.css';
@@ -21,6 +23,15 @@ const ScannerPage = () => {
         { value: 'qr', label: 'QR Code' },
         { value: 'barcode', label: 'Bar Code' },
     ];
+
+    const { data: session, status } = useSession(); // Use useSession to get session status
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === 'unauthenticated') {
+        router.push("/login") 
+        }
+    }, [status, router]);
 
     // Update localStorage whenever activeTab changes
     useEffect(() => {
@@ -42,22 +53,25 @@ const ScannerPage = () => {
         };
     }, []);
 
-    return (
-        <div className="scanner-container">
-            <h1 className="scanner-title">Barcode and QR Code Scanner</h1>
+    if (status === 'authenticated') {
+        return (
+            <div className="scanner-container">
+                <h1 className="scanner-title">Barcode and QR Code Scanner</h1>
 
-            <Tab
-                tabs={tabs}
-                onTabChange={setActiveTab}
-                className="mb-6"
-            />
+                <Tab
+                    tabs={tabs}
+                    onTabChange={setActiveTab}
+                    className="mb-6"
+                />
 
-            <div className="scanner-content">
-                {activeTab === 'qr' && <CameraScanner type="QR Code" />}
-                {activeTab === 'barcode' && <CameraScanner type="Bar Code" />}
+                <div className="scanner-content">
+                    {activeTab === 'qr' && <CameraScanner type="QR Code" />}
+                    {activeTab === 'barcode' && <CameraScanner type="Bar Code" />}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    return null;
 };
 
 export default ScannerPage;
